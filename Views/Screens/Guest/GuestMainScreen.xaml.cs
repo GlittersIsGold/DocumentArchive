356eddb15c4d610ee1065712b2c9803fa05c785d
GlittersIsGold <cyberk1ra@ya.ru>
2023-03-12 12:12:38 +0300

    навигация

--- Views/Screens/Guest/GuestMainScreen.xaml.cs
+++ Views/Screens/Guest/GuestMainScreen.xaml.cs
@@ -1,20 +1,6 @@
-﻿using DocumentArchive.Controller.Navigation;
-using DocumentArchive.Model.ADO;
-using DocumentArchive.Views.Pages.General.Document;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DocumentArchive.Views.Pages.General.Document;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DocumentArchive.Views.Screens.Guest
 {
@@ -41,6 +27,7 @@ namespace DocumentArchive.Views.Screens.Guest
 		private void BtnLogin_Click(object sender, RoutedEventArgs e)
 		{
 			new MainWindow().Show();
+			Window.GetWindow(this).Close();
 		}
     }
 }
