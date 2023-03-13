9cd87cdddd66b8953d7481f33d255b4b695ffcdd
GlittersIsGold <cyberk1ra@ya.ru>
2023-03-13 21:52:40 +0300

    Resolving merge troubles step 2

--- Views/Screens/Guest/GuestMainScreen.xaml.cs
+++ Views/Screens/Guest/GuestMainScreen.xaml.cs
@@ -3,8 +3,10 @@ using DocumentArchive.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace DocumentArchive.Views.Screens.Guest
 {
@@ -14,14 +16,56 @@ namespace DocumentArchive.Views.Screens.Guest
 	public partial class GuestMainScreen : Page
 	{
 		private DateTime InfoUpdated;
+		private int numRequestCalls = 0;
 		
 		public GuestMainScreen()
 		{
 			InitializeComponent();
 			
+			try
+			{
+
+				List<GuestFileInfo> DgData = DataAccess.EDAEntities.GuestFileInfo.ToList();
+
+				if (DgData != null)
+				{
+					if (DgData.Count > 0)
+					{
+						DgPublicFiles.ItemsSource = DgData;
+						LbPinnedFiles.ItemsSource = DgData;
+						InfoUpdated = DateTime.Now;
+						RnLastUpdate.Text = InfoUpdated.ToString("T");
+						RnCountFiles.Text = DgData.Count.ToString();
+						numRequestCalls++;
+					}
+					else
+					{
+						MessageBox.Show("Нет данных");
+					}
+				}
+				else
+				{
+					MessageBox.Show("Не удалось получить данные");
+					throw new ArgumentNullException(nameof(DgData));
+				}
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Возникла ошибка\nсмотрите подробности во внутреннем исключении");
+				throw new Exception(ex.Message);
+			}
+
+			DispatcherTimer dt = new DispatcherTimer();
+			dt.Tick += new EventHandler(dt_Tick);
+			dt.Interval = new TimeSpan(0, 0, 1);
+			dt.Start();
+
 			/// всё по классам
-			/// освобождать память
-			/// обновлять по времени
+		}
+
+		private void dt_Tick(object sender, EventArgs e)
+		{
 			try
 			{
 				
@@ -36,6 +80,7 @@ namespace DocumentArchive.Views.Screens.Guest
 						InfoUpdated = DateTime.Now;
 						RnLastUpdate.Text = InfoUpdated.ToString("T");
 						RnCountFiles.Text = DgData.Count.ToString();
+						numRequestCalls++;
 					}
 					else
 					{
@@ -47,6 +92,9 @@ namespace DocumentArchive.Views.Screens.Guest
 					MessageBox.Show("Не удалось получить данные");
 					throw new ArgumentNullException(nameof(DgData));
 				}
+
+				GC.Collect(1, GCCollectionMode.Forced);
+				GC.WaitForPendingFinalizers();
 			}
 			catch (Exception ex)
 			{
@@ -57,9 +105,8 @@ namespace DocumentArchive.Views.Screens.Guest
 
 		private void BtnOpenReader_Click(object sender, RoutedEventArgs e)
 		{
+
         }
-
-
 
 		private void BtnLogin_Click(object sender, RoutedEventArgs e)
 		{
